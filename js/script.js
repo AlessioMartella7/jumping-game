af9869cba82a97ed01662e0069da12da50149388
AlessioMartella7 <alessio.mart87@gmail.com>
2025-01-03 17:07:56 +0100

    refactored click on 'game' instead of document to trigger event

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
 const character = document.getElementById('character');
 const block = document.getElementById('block');
+const game = document.getElementById('game')
 
-document.addEventListener('click', jump);
+game.addEventListener('click', jump);
     function jump(){
         if(character.classList == 'animate'){return}
         character.classList.add('animate') //add class to character
