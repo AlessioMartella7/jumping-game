25b6b0fb2715df05ab2f2641dad3be69dda07ac9
AlessioMartella7 <alessio.mart87@gmail.com>
2025-01-03 16:56:27 +0100

    added jump function to character

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
-console.log('js ok')
+const character = document.getElementById('character');
+const block = document.getElementById('block');
+
+document.addEventListener('click', jump);
+    function jump(){
+        if(character.classList == 'animate'){return}
+        character.classList.add('animate') //add class to character
+            setTimeout(removeJump, 300); //remove class after action performed
+    }
+    function removeJump(){
+        character.classList.remove('animate');
+    }
