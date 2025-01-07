dec2fd95456cb6538e70b6d9708fd5fe78e6911c
AlessioMartella7 <alessio.mart87@gmail.com>
2025-01-07 19:57:22 +0100

    fixed jump on block element

--- js/script.js
+++ js/script.js
@@ -2,12 +2,18 @@ const character = document.getElementById('character');
 const block = document.getElementById('block');
 const game = document.getElementById('game')
 
-game.addEventListener('click', jump);
-    function jump(){
-        if(character.classList == 'animate'){return}
-        character.classList.add('animate') //add class to character
-            setTimeout(removeJump, 400); //remove class after action performed
+function jump(){
+    if(character.classList == 'animate'){
+        return
+    }else{
+    character.classList.add('animate') //add class to character
+        setTimeout(removeJump, 400); //remove class after action performed
     }
-    function removeJump(){
-        character.classList.remove('animate');
-    }
+}
+
+function removeJump(){
+    character.classList.remove('animate');
+}
+
+game.addEventListener('click', jump, false);
+
