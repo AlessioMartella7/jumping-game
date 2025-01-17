d7a598871722442a3306831d023caa826fef28a8
AlessioMartella7 <alessio.mart87@gmail.com>
2025-01-17 17:05:57 +0100

    added collision function

--- js/script.js
+++ js/script.js
@@ -24,11 +24,25 @@ this.color = color;
 var block2 = new Block(3.5,[220,39,221])
 var Block2speed = block2.speed;
 var Block2color = block2.color
-var elBlock2 = document.getElementById('block-2');
+var elBlock2 = document.getElementById('block2');
 var animationBlock2Speed = elBlock2.style.animationDuration = Block2speed+'s';
-var bgBlock2Color = elBlock2.style.backgroundColor = 'rgb('+ [Block2color].join(';') + ')';
+var bgBlock2Color = elBlock2.style.backgroundColor = 'rgb('+ [Block2color].join(',') + ')';
 
 game.addEventListener('click', jump, false);
 
+// collision
 
+function collision(){
+    let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue('top'));
+    let blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue('left'));
+    let block2Left = parseInt(window.getComputedStyle(block2).getPropertyValue('left'));
+
+    if(!(characterTop + 70 < 376 || 
+        characterTop + 70 < 366 ||
+        (blockLeft || block2Left )> 370 + 30
+        )
+      ) {
+        return true
+        }
+}
 
